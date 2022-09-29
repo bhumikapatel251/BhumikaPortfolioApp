9fafe1f53bd36f934be3d946f6783b5e55c48194
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-09-29 15:45:13 +0530

    Initial Commit

---
+++ BhumikaPortfolioApp/BhumikaPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhumikaPortfolioAppApp.swift
+//  BhumikaPortfolioApp
+//
+//  Created by Bhumika Patel on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BhumikaPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
